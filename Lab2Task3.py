dd8b1ef7b293b01f030194da700ed5d0d8f3a0c3
mattmhk <valorantsmurfinnit@gmail.com>
2026-01-23 10:59:33 +0000

    Fix delivery cost calculation by adjusting order quantity in Lab2Task3.py

--- Lab2Task3.py
+++ Lab2Task3.py
@@ -4,7 +4,7 @@ fixed_delivery_cost = 5.50
 unit_delivery_cost = 0.55
 order_qty = 350
 cost_of_books = cover_price * order_qty * (100 - discount_perc) / 100
-delivery_cost = fixed_delivery_cost + unit_delivery_cost * order_qty
+delivery_cost = fixed_delivery_cost + unit_delivery_cost * (order_qty-1)
 print("The final order price is :", cost_of_books + delivery_cost)
 print("The order details are as follows:")
 print("Cost of books: $", cost_of_books)
